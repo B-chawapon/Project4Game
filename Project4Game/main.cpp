a2a227a6bf98ce26d1831036e54f24957b22a157
Chawapon <chawapon2001@gmail.com>
2020-10-22 01:44:14 +0700

    update water boat
    update water and boat
    fix speed car
    update item sprite clock

--- Project4Game/main.cpp
+++ Project4Game/main.cpp
@@ -16,7 +16,6 @@ void ResizeView(const sf::RenderWindow& window, sf::View& view)
 {
 	float aspectRatio = float(window.getSize().x / float(window.getSize().y));
 	view.setSize(screenheight * aspectRatio, screenheight);
-
 }
 
 int main()
@@ -25,7 +24,7 @@ int main()
 
 	sf::Vector2i screen(1080, 720);
 	sf::RenderWindow window(sf::VideoMode(screen.x, screen.y), "GAME!", sf::Style::Close | sf::Style::Resize);
-	
+
 	sf::RectangleShape player(sf::Vector2f(20.f, 20.f));
 	player.setFillColor(sf::Color::Green);
 
@@ -59,27 +58,45 @@ int main()
 
 	sf::RectangleShape boat1(sf::Vector2f(200.f, 62.5f));
 	boat1.setFillColor(sf::Color::Red);
-	boat1.setPosition(0, 360.0f);
+	boat1.setPosition(-100, 360.0f);
 	float xboat1 = 0;
 	float yboat1 = rand() % 548;
 
 	sf::RectangleShape boat2(sf::Vector2f(220.f, 62.5f));
 	boat2.setFillColor(sf::Color::Blue);
-	boat2.setPosition(0, 422.5f);
+	boat2.setPosition(-100, 422.5f);
 	float xboat2 = 0;
 	float yboat2 = rand() % 548;
 
 	sf::RectangleShape boat3(sf::Vector2f(230.f, 62.5f));
 	boat3.setFillColor(sf::Color::Black);
-	boat3.setPosition(0, 485.0f);
+	boat3.setPosition(1150, 485.0f);
 	float xboat3 = 0;
 	float yboat3 = rand() % 548;
 
 	sf::RectangleShape boat4(sf::Vector2f(225.f, 62.5f));
 	boat4.setFillColor(sf::Color::White);
-	boat4.setPosition(0, 547.5f);
+	boat4.setPosition(1150, 547.5f);
 	float xboat4 = 0;
 	float yboat4 = rand() % 548;
+
+	sf::Texture textureclock;
+	textureclock.loadFromFile("clock.png");
+	textureclock.setSmooth(true);
+
+	sf::Sprite itemclock(textureclock,sf::IntRect(0,0,400,400));
+	itemclock.setTexture(textureclock);
+	itemclock.setPosition(20.0f,20.0f);
+	itemclock.setScale(sf::Vector2f(0.1f,0.1f));
+
+
+	
+
+	
+
+
+
+
 
 	sf::View view;
 	view.reset(sf::FloatRect(0, 0, screen.x, screen.y));
@@ -109,236 +126,8 @@ int main()
 	yellow.setPosition(xci4, yci4);
 
 
-	
-
 	while (window.isOpen())
 	{
-		window.clear();
-		
-		
-		
-		positionview.y = player.getPosition().y + 10 - (screen.y / 2);
-		positionview.x = 0;
-		if (positionview.y < 0)
-		{
-			positionview.y = 0;
-		}
-		else if (player.getPosition().y >= 4650)
-		{
-			positionview.y = 4300;
-		}
-		view.reset(sf::FloatRect(positionview.x, positionview.y, screen.x, screen.y));
-
-		
-
-		purple.setPosition(0, positionview.y);
-		purple2.setPosition(0, positionview.y + 710);
-
-
-		sf::Vector2f movement(0.f, 0.f);
-		//white
-		if (checksidexci == 0)
-		{
-			xci = -50;
-			white.move(25.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			
-			if (white.getPosition().x >= 1150 && checkcollin !=1)
-			{
-				checksidexci = rand() % 2;
-				yci = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci >= positionview.y) && (yci <= (positionview.y + 710)))
-					{
-						white.setPosition(xci, yci);
-						break;
-					}
-					else
-					{
-						yci = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		else if (checksidexci == 1) {
-			xci = 1150;
-			white.move(-25.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			if (white.getPosition().x < -50)
-			{
-				checksidexci = rand() % 2;
-				yci = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci >= positionview.y) && (yci <= (positionview.y + 710)))
-					{
-						white.setPosition(xci, yci);
-						break;
-					}
-					else
-					{
-						yci = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		//green
-		if (checksidexci2 == 0)
-		{
-			xci2 = -50;
-			green.move(30.0f, 0.0f);
-
-			//printf("%.2f \n", collision.getPosition().x);
-			if (green.getPosition().x >= 1150)
-			{
-				checksidexci2 = rand() % 2;
-				yci2 = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci2 >= positionview.y) && (yci2 <= (positionview.y + 710)))
-					{
-						green.setPosition(xci2, yci2);
-						break;
-					}
-					else
-					{
-						yci2 = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		else if (checksidexci2 == 1) {
-			xci2 = 1150;
-			green.move(-30.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			if (green.getPosition().x < -50)
-			{
-				checksidexci2 = rand() % 2;
-				yci2 = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci2 >= positionview.y) && (yci2 <= (positionview.y + 710)))
-					{
-						green.setPosition(xci2, yci2);
-						break;
-					}
-					else
-					{
-						yci2 = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		//red
-		if (checksidexci3 == 0)
-		{
-			xci3 = -150;
-			red.move(40.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			if (red.getPosition().x >= 1150)
-			{
-				checksidexci3 = rand() % 2;
-				yci3 = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci3 >= positionview.y) && (yci3 <= (positionview.y + 710)))
-					{
-						red.setPosition(xci3, yci3);
-						break;
-					}
-					else
-					{
-						yci3 = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		else if (checksidexci3 == 1) {
-			xci3 = 1140;
-			red.move(-40.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			if (red.getPosition().x < -50)
-			{
-				checksidexci3 = rand() % 2;
-				yci3 = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci3 >= positionview.y) && (yci3 <= (positionview.y + 710)))
-					{
-						red.setPosition(xci3, yci3);
-						break;
-					}
-					else
-					{
-						yci3 = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		//yellow
-		if (checksidexci4 == 0)
-		{
-			xci4 = -150;
-			yellow.move(40.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			if (yellow.getPosition().x >= 1150)
-			{
-				checksidexci4 = rand() % 2;
-				yci4 = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci4 >= positionview.y) && (yci4 <= (positionview.y + 710)))
-					{
-						yellow.setPosition(xci4, yci4);
-						break;
-					}
-					else
-					{
-						yci4 = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		else if (checksidexci4 == 1) {
-			xci4 = 1140;
-			yellow.move(-40.0f, 0.0f);
-			//printf("%.2f \n", collision.getPosition().x);
-			if (yellow.getPosition().x < -50)
-			{
-				checksidexci4 = rand() % 2;
-				yci4 = rand() % 4801;
-				for (j = 0; j != i; )
-				{
-					if ((yci4 >= positionview.y) && (yci4 <= (positionview.y + 710)))
-					{
-						yellow.setPosition(xci4, yci4);
-						break;
-					}
-					else
-					{
-						yci4 = rand() % 4801;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -354,36 +143,204 @@ int main()
 		}
 
 
-		//boat
-		//boat1****************************************************
-			xboat1 = -80;
-			boat1.move(2.2f, 0.0f);
-			if (boat1.getPosition().x >= 1150 && checkcollin != 1)
+		positionview.y = player.getPosition().y + 10 - (screen.y / 2);
+		positionview.x = 0;
+		if (positionview.y < 0)
+		{
+			positionview.y = 0;
+		}
+		else if (player.getPosition().y >= 4650)
+		{
+			positionview.y = 4300;
+		}
+		view.reset(sf::FloatRect(positionview.x, positionview.y, screen.x, screen.y));
+
+		purple.setPosition(0, positionview.y);
+		purple2.setPosition(0, positionview.y + 710);
+
+
+		sf::Vector2f movement(0.f, 0.f);
+		//white
+		if (checksidexci == 0)
+		{
+			xci = -50;
+			white.move(10.0f, 0.0f);
+			if (white.getPosition().x >= 1150 && checkcollin != 1)
 			{
-				yboat1 = rand() % 548;
+				checksidexci = rand() % 2;
+				yci = rand() % 4801;
 				for (j = 0; j != i; )
 				{
-					if (yboat1 == 360.0f /*|| yboat1==422.5f || yboat1==485.0f || yboat1==547.5*/)
+					if ((yci >= positionview.y) && (yci <= (positionview.y + 710)) && !(yci>=310 && yci<=610) && yci>30 )
 					{
-						boat1.setPosition(xboat1, yboat1);
+						white.setPosition(xci, yci);
 						break;
 					}
-					else {
-						yboat1 = rand() % 548;
-							i++;
-							j++;
+					else
+					{
+						yci = rand() % 4801;
 					}
 				}
 			}
-			
-		
-		
-
+		}
+		else if (checksidexci == 1) {
+			xci = 1150;
+			white.move(-10.0f, 0.0f);
+			if (white.getPosition().x < -50)
+			{
+				checksidexci = rand() % 2;
+				yci = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci >= positionview.y) && (yci <= (positionview.y + 710)) && !(yci >= 310 && yci <= 610) && yci > 30)
+					{
+						white.setPosition(xci, yci);
+						break;
+					}
+					else
+					{
+						yci = rand() % 4801;
+					}
+				}
+			}
+		}
+		//green
+		if (checksidexci2 == 0)
+		{
+			xci2 = -50;
+			green.move(18.0f, 0.0f);
+			if (green.getPosition().x >= 1150)
+			{
+				checksidexci2 = rand() % 2;
+				yci2 = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci2 >= positionview.y) && (yci2 <= (positionview.y + 710)) && !(yci2 >= 310 && yci2 <= 610) && yci2 > 30)
+					{
+						green.setPosition(xci2, yci2);
+						break;
+					}
+					else
+					{
+						yci2 = rand() % 4801;
+					}
+				}
+			}
+		}
+		else if (checksidexci2 == 1) {
+			xci2 = 1150;
+			green.move(-18.0f, 0.0f);
+			if (green.getPosition().x < -50)
+			{
+				checksidexci2 = rand() % 2;
+				yci2 = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci2 >= positionview.y) && (yci2 <= (positionview.y + 710)) && !(yci2 >= 310 && yci2 <= 610) && yci2 > 30)
+					{
+						green.setPosition(xci2, yci2);
+						break;
+					}
+					else
+					{
+						yci2 = rand() % 4801;
+					}
+				}
+			}
+		}
+		//red
+		if (checksidexci3 == 0)
+		{
+			xci3 = -150;
+			red.move(15.0f, 0.0f);
+			if (red.getPosition().x >= 1150)
+			{
+				checksidexci3 = rand() % 2;
+				yci3 = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci3 >= positionview.y) && (yci3 <= (positionview.y + 710)) && !(yci3 >= 310 && yci3 <= 610) && yci3 > 30)
+					{
+						red.setPosition(xci3, yci3);
+						break;
+					}
+					else
+					{
+						yci3 = rand() % 4801;
+					}
+				}
+			}
+		}
+		else if (checksidexci3 == 1) {
+			xci3 = 1140;
+			red.move(-15.0f, 0.0f);
+			if (red.getPosition().x < -50)
+			{
+				checksidexci3 = rand() % 2;
+				yci3 = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci3 >= positionview.y) && (yci3 <= (positionview.y + 710)) && !(yci3 >= 310 && yci3 <= 610) && yci3 > 30)
+					{
+						red.setPosition(xci3, yci3);
+						break;
+					}
+					else
+					{
+						yci3 = rand() % 4801;
+					}
+				}
+			}
+		}
+		//yellow
+		if (checksidexci4 == 0)
+		{
+			xci4 = -150;
+			yellow.move(13.0f, 0.0f);
+			if (yellow.getPosition().x >= 1150)
+			{
+				checksidexci4 = rand() % 2;
+				yci4 = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci4 >= positionview.y) && (yci4 <= (positionview.y + 710)) && !(yci4 >= 310 && yci4 <= 610) && yci4 > 30)
+					{
+						yellow.setPosition(xci4, yci4);
+						break;
+					}
+					else
+					{
+						yci4 = rand() % 4801;
+					}
+				}
+			}
+		}
+		else if (checksidexci4 == 1) {
+			xci4 = 1140;
+			yellow.move(-13.0f, 0.0f);
+			if (yellow.getPosition().x < -50)
+			{
+				checksidexci4 = rand() % 2;
+				yci4 = rand() % 4801;
+				for (j = 0; j != i; )
+				{
+					if ((yci4 >= positionview.y) && (yci4 <= (positionview.y + 710)) && !(yci4 >= 310 && yci4 <= 610) && yci4 > 30)
+					{
+						yellow.setPosition(xci4, yci4);
+						break;
+					}
+					else
+					{
+						yci4 = rand() % 4801;
+					}
+				}
+			}
+		}
 
 		//player move  //+ Collinsions *************************************************************
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
 		{
-			player.move(0.f * speed, -5.0f * speed);
+			player.move(0.f * speed, -3.0f * speed);
 			/*if (player.getGlobalBounds().intersects(box.getGlobalBounds()))
 			{
 				player.move(0.f * speed, +5.0f * speed);
@@ -391,7 +348,7 @@ int main()
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
 		{
-			player.move(0.f * speed, 5.0f * speed);
+			player.move(0.f * speed, 3.0f * speed);
 			/*if (player.getGlobalBounds().intersects(box.getGlobalBounds()))
 			{
 				player.move(0.f * speed, -5.0f * speed);
@@ -399,7 +356,7 @@ int main()
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
 		{
-			player.move(-5.0f * speed, 0.0f * speed);
+			player.move(-3.0f * speed, 0.0f * speed);
 			/*if (player.getGlobalBounds().intersects(box.getGlobalBounds()))
 			{
 				player.move(+5.0f * speed, 0.0f * speed);
@@ -407,7 +364,7 @@ int main()
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 		{
-			player.move(5.0f * speed, 0.f * speed);
+			player.move(3.0f * speed, 0.f * speed);
 			/*if (player.getGlobalBounds().intersects(box.getGlobalBounds()))
 			{
 				player.move(-5.0f * speed, 0.0f * speed);
@@ -423,7 +380,7 @@ int main()
 		}
 
 		//Collinsions
-		/*if (white.getGlobalBounds().intersects(player.getGlobalBounds())) {
+		if (white.getGlobalBounds().intersects(player.getGlobalBounds())) {
 			//shape.setPosition(spawnPoint);
 			speed -= 0.1;
 		}
@@ -438,32 +395,87 @@ int main()
 		if (yellow.getGlobalBounds().intersects(player.getGlobalBounds())) {
 			//shape.setPosition(spawnPoint);
 			speed -= 0.1;
-		}*/
+		}
 
-		//Colin Boat
+		//Colin player no boat
 		if (player.getGlobalBounds().intersects(box.getGlobalBounds()) && !(player.getGlobalBounds().intersects(boat1.getGlobalBounds())) && !(player.getGlobalBounds().intersects(boat2.getGlobalBounds())) && !(player.getGlobalBounds().intersects(boat3.getGlobalBounds())) && !(player.getGlobalBounds().intersects(boat4.getGlobalBounds())))
 		{
-
 			player.setPosition(spawnPoint);
-
-
 		}
-		/*if (player.getGlobalBounds().intersects(boat1.getGlobalBounds()) && player.getGlobalBounds().intersects(box.getGlobalBounds()))
+
+		//boat****************************************************
+		//boat1
+		xboat1 = -80;
+		boat1.move(2.2f, 0.0f);
+		if (boat1.getPosition().x >= 1150)
 		{
-			float dtback1 =(player.getPosition().x + player.getSize().x )-( boat1.getPosition().x + boat1.getSize().x);
-			if (boat1.getPosition().x - player.getPosition().x >17.5f || dtback1>17.5f )
+			yboat1 = rand() % 548;
+			for (j = 0; j != i; )
 			{
-				printf("x%f\n ", player.getPosition().x);
-				printf("boat%f ", boat1.getPosition().x);
-				player.setPosition(spawnPoint);
-
-				
+				if (yboat1 == 360.0f /*|| yboat1==422.5f || yboat1==485.0f || yboat1==547.5*/)
+				{
+					boat1.setPosition(xboat1, yboat1);
+					break;
+				}
+				else {
+					yboat1 = rand() % 548;
+				}
 			}
-			
-		}*/
-		
-
-
+		}
+		//boat2
+		xboat2 = -80;
+		boat2.move(2.3f, 0.0f);
+		if (boat2.getPosition().x >= 1150)
+		{
+			yboat2 = rand() % 548;
+			for (j = 0; j != i;)
+			{
+				if (yboat2 == 423.f)
+				{
+					boat2.setPosition(xboat2, yboat2);
+					break;
+				}
+				else {
+					yboat2 = rand() % 548;
+				}
+			}
+		}
+		//boat3
+		xboat3 = 1150;
+		boat3.move(-1.9f, 0.0f);
+		if (boat3.getPosition().x <= -350)
+		{
+			yboat3 = rand() % 548;
+			for (j = 0; j != i;)
+			{
+				if (yboat3 == 485.f)
+				{
+					boat3.setPosition(xboat3, yboat3);
+					break;
+				}
+				else {
+					yboat3 = rand() % 548;
+				}
+			}
+		}
+		//boat4
+		xboat4 = 1150;
+		boat4.move(-2.0f, 0.0f);
+		if (boat4.getPosition().x <= -350)
+		{
+			yboat4 = rand() % 549;
+			for (j = 0; j != i;)
+			{
+				if (yboat4 == 548.0f)
+				{
+					boat4.setPosition(xboat4, yboat4);
+					break;
+				}
+				else {
+					yboat4 = rand() % 549;
+				}
+			}
+		}
 
 		//check in window
 		if (player.getPosition().x < 0) {
@@ -482,14 +494,17 @@ int main()
 			player.setPosition(player.getPosition().x, 5000);
 		}
 
+		
 
-		
+		window.clear();
+
+
 		window.setView(view);
-		
-		
+
+
 		window.draw(white);
-		window.draw(purple);
-		window.draw(purple2);
+		/*window.draw(purple);
+		window.draw(purple2);*/
 		window.draw(green);
 		window.draw(red);
 		window.draw(yellow);
@@ -498,7 +513,7 @@ int main()
 		window.draw(boat2);
 		window.draw(boat3);
 		window.draw(boat4);
-		
+		window.draw(itemclock);
 		window.draw(player);
 		window.display();
 
